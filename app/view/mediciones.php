97308952dffb88953525994f0426d5e21d72cb7b
luiseurban <165191210+luiseurban@users.noreply.github.com>
2026-01-29 21:38:19 -0500

    Update mediciones.php

--- app/view/mediciones.php
+++ app/view/mediciones.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mediciones Ambientales - GreenGrid 360</title>
-    <link rel="stylesheet" href="../../css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
 </head>
 <body>
     <div class="container">

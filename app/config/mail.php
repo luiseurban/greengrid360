6120bb2e21208193ed66b3c408d37f1463218bd0
LuiseUrbano <aprendizdevelopment@uni2.com.co>
2026-05-29 10:50:38 -0500

    Cambio de correo

--- app/config/mail.php
+++ app/config/mail.php
@@ -3,8 +3,8 @@
 return [
     'smtp_host' => 'smtp.gmail.com',
     'smtp_port' => 587,
-    'smtp_user' => 'greengrid360@gmail.com',
-    'smtp_pass' => 'fpre kiic nenx iwpg',
-    'from_email' => 'greengrid360@gmail.com',
+    'smtp_user' => 'GreenGrid360s@gmail.com',
+    'smtp_pass' => 'kpwr gdlq ygxq ulzn',
+    'from_email' => 'GreenGrid360s@gmail.com',
     'from_name' => 'GreenGrid 360',
 ];
